61f7dfe56a16cfe4d3c79777ba9be0886ccdc357
Enrique Star <chetyenrique@gmail.com>
2025-10-23 19:49:56 -0300

    Fix: usar host y puerto para Render

--- app.py
+++ app.py
@@ -57,4 +57,7 @@ def upload_file():
     return render_template_string(HTML_FORM)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
